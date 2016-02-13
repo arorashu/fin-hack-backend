385c81046bb8a85a7a748484ef7975f3be319600
Shubham Arora <shubhama1903@gmail.com>
2016-02-13 13:17:13 +0530

    user account model created

--- models/Account.js
+++ models/Account.js
@@ -1,3 +1,55 @@
 /**
  * Created by shubham on 13/2/16.
  */
+
+var mongoose = require('mongoose');
+var Schema = mongoose.Schema;
+var passportLocalMongoose = require('passport-local-mongoose');
+var validate = require('mongoose-validator');
+var autoIncrement = require('mongoose-auto-increment');
+
+var connection = mongoose.createConnection('mongodb://127.0.0.1:27017/innovision');
+autoIncrement.initialize(connection);
+
+var nameValidator = [
+    validate({
+        validator: 'isLength',
+        arguments: [3, 50],
+        message: 'Name should be between 3 and 50 characters'
+    })
+];
+var emailValidator=[
+    validate({
+        validator: 'isEmail',
+        message: "not a valid email"
+    })
+];
+var phoneValidator = [
+    validate({
+        validator: 'isLength',
+        arguments: [10, 10],
+        message: 'phonenumber should be 10 digits'
+    })
+];
+
+var Account = new Schema({
+    accNo: Number,
+    provider: String,
+    providerData: Object,
+    accessToken: String,
+    password: String,
+    email: {type:String, validator:emailValidator, unique:true, dropDups:true, trim: true},
+    firstName: String,
+    lastName: String,
+    photo: String,
+    dob: Date,
+    gender: String,
+    phone_no: {type:String, validate:phoneValidator, unique:true, dropDups:true, sparse:true, trim: true},
+    dateJoined: Date,
+    is_new: {type:Boolean,default:true}
+});
+
+Account.plugin(autoIncrement.plugin, {model: 'Account', field: 'accNo'});
+Account.plugin(passportLocalMongoose, {usernameField: 'email', usernameLowerCase: true});
+
+module.exports = mongoose.model('Account', Account);
--- routes/index.js
+++ routes/index.js
@@ -1,4 +1,5 @@
 var express = require('express');
+var passport = require('passport');
 var router = express.Router();
 
 /* GET home page. */
@@ -9,10 +10,30 @@ router.get('/', function(req, res, next) {
 router.get('/login/fb', passport.authenticate('facebook',
     {authType: 'rerequest', scope: ['email', 'user_birthday']}));
 
-router.post('/login', function (req,res) {
+router.get('/login/fb/callback',
+    passport.authenticate('facebook', {
+      failureRedirect: '/login'
+    }), function(req, res) {
+      if (req.user.is_new) {
+        res.redirect('/users/details');
+      } else {
+        res.redirect('/');
+      }
+    }, function(err, req, res) {
+      if(err) {
+        req.logout();
+        res.json({logout: true} );
+      }
+    }
+);
 
+router.post('/login', passport.authenticate('local', { failureRedirect: '/login' }), function (req, res) {
+  res.json({login: true});
+});
 
-
+router.get('/logout', function (req, res) {
+  req.logout();
+  res.json({logout: true});
 });
 
 module.exports = router;
